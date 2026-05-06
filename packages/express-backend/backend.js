ffa12c88cef94d66d60a9f435d07c3176e219401
Kaito-Rushworth <kai.rushworth@gmail.com>
2026-05-05 20:37:05 -0700

    IA4 DRC Deadline Extension Approved by Professor

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -2,121 +2,82 @@
 import express from "express";
 import cors from "cors";
 
+import userServices from "./models/user-services.js";
+
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
-
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-const generateId = () => {
-  return Math.random().toString(36).substring(2, 10);
-};
-
-const addUser = (user) => {
-  user["id"] = generateId()
-  users["users_list"].push(user);
-  return user;
-};
-
-const deleteUser = (id) => {
-  const index = users["users_list"].findIndex(
-    (user) => user["id"] === id
-  );
-
-  users["users_list"].splice(index, 1);
-};
-
-app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  const addedUser = addUser(userToAdd);
-  res.status(201).send(addedUser);
-});
-
-app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"];
-  const index = users["users_list"].findIndex(
-    (user) => user["id"] === id
-  );
-  if (index === -1) {
-    res.status(404).send("Not found!! >:(");
-  } else {
-    users["users_list"].splice(index, 1);
-    res.status(204).send();
-  }
-});
-
-app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
-});
-
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("I Love Chips!!");
 });
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  const job = req.query.job;
+app.get("/users", async (req, res) => {
+  const name = req.query["name"];
+  const job = req.query["job"];
 
-  if (name != undefined && job != undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  } else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
+  try {
+    const result = await userServices.getUsers(name, job);
+    res.send({ users_list: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+
+  try {
+    const result = await userServices.findUserById(id);
+
+    if (result === undefined || result === null) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
+  }
+});
+
+app.post("/users", async (req, res) => {
+  const user = req.body;
+
+  try {
+    const savedUser = await userServices.addUser(user);
+
+    if (savedUser) {
+      res.status(201).send(savedUser);
+    } else {
+      res.status(500).end();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+
+  try {
+    const deletedUser = await userServices.deleteUser(id);
+
+    if (deletedUser === null) {
+      res.status(404).send("Not found!! >:(");
+    } else {
+      res.status(204).send();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
   }
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+  console.log(`Example app listening at http://localhost:${port}`);
 });
